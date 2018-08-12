33dcf32aeac619e592a2f07b26b92e70570bc50d
Guillermo Garcia Juanes <ggjuanes@gmail.com>
2018-08-12 18:41:24 +0200

    First beta version of the script to execute from shell

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
-main();
-function main() {
+import WeightCounter from "./Context/Word/Module/Counter/Application/WeightCounter";
+import Word from "./Context/Word/Module/Counter/Domain/Word";
+
+const weightCounter = new WeightCounter();
+
+if (process.argv.length <= 2) {
   // tslint:disable-next-line:no-console
-   console.log(`Hello Typescript!`);
+  console.log("Word is requred");
+  process.exit(-1);
 }
+const word = new Word(process.argv[2]);
+const amount = weightCounter.count(word);
+
+// tslint:disable-next-line:no-console
+console.log("Amount for the word (" + word.getValue() + ") is: " + amount);
